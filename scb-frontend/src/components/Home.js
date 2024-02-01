3830531a422e8befb71228f8ecd5e6df75d69921
StillABeginnerr <shivamagarwaloff@gmail.com>
2024-02-02 01:09:43 +0530

    done

--- scb-frontend/src/components/Home.js
+++ scb-frontend/src/components/Home.js
@@ -7,7 +7,9 @@ import AboutSym from './Header/AboutSym'
 import OrganisingCommittee from './Header/OrganisingCommittee'
 import PlacesToExplore from './Header/PlacesToExplore'
 import EventDetails from './Header/EventDetails'
+import Brochure from '../../src/components/OOO.pdf'
 import './home.css'
+import downloadd from '../components/download.png'
 
 function openWhatsApp() {
   const whatsappLink = "https://api.whatsapp.com/send/?phone=919777492429&text=Hello%2C%0D%0AI%27m+having+a+query&type=phone_number&app_absent=0";
@@ -25,6 +27,7 @@ function Home() {
       <AboutSym></AboutSym>
       <PlacesToExplore></PlacesToExplore>
       <OrganisingCommittee></OrganisingCommittee>
+      <a href={Brochure} download><img className='whatsapps-btn' src={downloadd} style={{ width: '50px', height: '50px' }}></img></a>
       <img className='whatsapp-btn' src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" onClick={openWhatsApp} style={{ width: '50px', height: '50px' }}></img>
     </div>
   )
